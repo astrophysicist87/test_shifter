57b1ebdd1d28b8a92b6f5e296e5f54464f4a7569
Christopher Plumberg <plumberg.1@osu.edu>
2020-03-06 12:04:17 +0100

    Update random_events.h
    Added sgn function

--- include/random_events.h
+++ include/random_events.h
@@ -13,6 +13,12 @@
 
 using namespace std;
 
+template <class T>
+inline T
+sgn(T v) {
+    return T(v > T(0)) - T(v < T(0));
+}
+
 void generate_events(vector<ParticleRecord> & allParticles, ParameterReader * paraRdr)
 {
 	//cout << "Using random number generator for toy model calculation!" << endl;
