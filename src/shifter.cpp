6d9c56b45d7c716733c7a8933e550e677440178d
Christopher Plumberg <plumberg.1@osu.edu>
2020-03-18 14:40:55 +0100

    Update shifter.cpp

--- src/shifter.cpp
+++ src/shifter.cpp
@@ -33,7 +33,7 @@ namespace shift_lib
 	void shifter::initialize_all( ParameterReader * paraRdr_in,
 		vector<ParticleRecord> & allParticles_in )
 	{
-		include_pair_density = true;
+		include_pair_density = false;
 
 		// Load parameters.
 		paraRdr			= paraRdr_in;
@@ -134,7 +134,7 @@ namespace shift_lib
 	
 
 
-		constexpr bool perform_compensation = false;
+		constexpr bool perform_compensation = true;
 
 		// Iterate compensation shift until convergence.
 		int iStep = 0;
