ad3d69abaa8b23be8613ae6f3413af9fa5c6d5b6
Christopher Plumberg <astrophysicist87@gmail.com>
2023-02-18 12:59:46 -0800

    Typos

--- submit_job.sh
+++ submit_job.sh
@@ -5,11 +5,11 @@ sbatch <<EOT
 #SBATCH -t 72:00:00
 #SBATCH --nodes=1
 
-RESULTS_DIRECTORY=results-mult${1}-Nev${2}-norm${3}-nL${4}
+RESULTS_DIRECTORY=results-mult$1-Nev$2-norm$3-nL$4
 
 mkdir -p \${RESULTS_DIRECTORY}
 
-./shifter.e RNG_mult=${1} RNG_nLoops=${2} shifter_norm=${3} shifter_nLoops=${4} \
+./shifter.e RNG_mult=$1 RNG_nLoops=$2 shifter_norm=$3 shifter_nLoops=$4 \
             2> \${RESULTS_DIRECTORY}/run.out | grep ^OUT | awk '{print $4, $5}' \
             1> \${RESULTS_DIRECTORY}/pairs.out
 
