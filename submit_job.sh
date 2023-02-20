1b073e4936cd32dddd33633129030950f23cb312
Christopher Plumberg <astrophysicist87@gmail.com>
2023-02-20 08:00:42 -0800

    Uncollected output to /dev/null

--- submit_job.sh
+++ submit_job.sh
@@ -13,7 +13,7 @@ RESULTS_DIRECTORY=results-mult$1-Nev$2-norm$3-nL$4
 mkdir -p \${RESULTS_DIRECTORY}
 
 ./shifter.e RNG_mult=$1 RNG_nLoops=$2 shifter_norm=$3 shifter_nLoops=$4 \
-            2> \${RESULTS_DIRECTORY}/run.out | grep ^OUT | awk '{print \$4, \$5}' \
+            2> /dev/null | grep ^OUT | awk '{print \$4, \$5}' \
             1> \${RESULTS_DIRECTORY}/pairs.out
 
 exit 0
