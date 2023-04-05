8207c38c6e784fe921d9d169b00b17ddff4e10fc
Christopher Plumberg <astrophysicist87@gmail.com>
2023-04-05 10:15:18 -0700

    Added some new shift_modes to make systematic checking easier

--- src/shifter.cpp
+++ src/shifter.cpp
@@ -183,6 +183,25 @@ double shifter::get_probability( const double R, const vector<double> & pair_qzs
 		return result;
 	}
 	//--------------------------------------------------------------------------
+	else if ( SHIFT_MODE == "TRIAL2b" )
+	{
+		// use np (np-1 independent) pairs: 0:1, 1:2, 2:3, ..., np-1:np
+		const int n = pair_qzs.size();
+		const int np = static_cast<int>(0.5*(1.0+sqrt(1.0+8.0*n)));
+		double result = 1.0;
+		double normalization = paraRdr->getVal("shifter_norm");
+		int i = -1;
+		for (int i1 = 0; i1 < np - 1; ++i1)
+		for (int i2 = i1 + 1; i2 < np; ++i2)
+		{
+			i++;
+			bool include_this_pair = (i2 == i1+1) || (i1 == 0 && i2 == np-1);
+			if (!include_this_pair) continue;
+			result *= 1.0 + 0.5*(np-1.)*normalization*exp(-0.5*pair_qzs[i]*pair_qzs[i]*R*R);
+		}
+		return result;
+	}
+	//--------------------------------------------------------------------------
 	else if ( SHIFT_MODE == "TRIAL3" )
 	{
 		// use only np-1 independent pairs, and cycle over which gets omitted
@@ -273,6 +292,28 @@ double shifter::get_probability( const double R, const vector<double> & pair_qzs
 
 		return (sum / term_count);
 	}
+//--------------------------------------------------------------------------
+else if ( SHIFT_MODE == "TRIAL5" )
+{
+	// use only np-1 independent pairs, and cycle over which gets omitted
+	const int n = pair_qzs.size();
+	const int np = static_cast<int>(0.5*(1.0+sqrt(1.0+8.0*n)));
+	double result = 1.0;
+	double normalization = paraRdr->getVal("shifter_norm");
+	int i = -1;
+	double factor = 0.0;
+	for (int i1 = 0; i1 < np - 1; ++i1)
+	for (int i2 = i1 + 1; i2 < np; ++i2)
+	{
+		i++;
+		bool include_this_pair = (i2 == i1+1) || (i1 == 0 && i2 == np-1);
+		if (!include_this_pair) continue;
+		double term = 1.0 + 0.5*np*normalization*exp(-0.5*pair_qzs[i]*pair_qzs[i]*R*R);
+		result *= term;
+		factor += 1.0/term;
+	}
+	return factor*result/np;
+}
 	//--------------------------------------------------------------------------
 	else if ( SHIFT_MODE == "RMSscale" )
 	{
