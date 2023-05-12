5074cd4b8682e27fa480f9ef0419bfc0e5baf2a4
Christopher Plumberg <astrophysicist87@gmail.com>
2023-05-12 16:07:16 -0700

    Add class to sample 3D distribution easily

---
+++ include/distribution_3D.h
@@ -0,0 +1,97 @@
+#ifndef DISTRIBUTION_3D_H
+#define DISTRIBUTION_3D_H
+
+#include <chrono>
+#include <cmath>
+#include <cstdlib>
+#include <functional>
+#include <iostream>
+#include <random>
+#include <string>
+#include <unordered_map>
+#include <variant>
+
+using namespace std;
+
+typedef std::unordered_map<std::string,std::variant<bool, double>> param_list;
+
+class distribution_3D
+{
+
+private:
+
+	bool do_x = false, do_y = false, do_z = false;
+	double scale = 0.0, width = 0.0;
+
+	default_random_engine generator;
+	normal_distribution<double> normal_dist;
+	uniform_real_distribution<double> pMag_dist;
+	uniform_real_distribution<double> pTheta_dist;
+	uniform_real_distribution<double> pPhi_dist;
+
+	ostream & out;
+	ostream & err;
+
+	std::tuple<double,double,double> generate_sample_Gaussian()
+	{
+		double px = do_x ? normal_dist(generator) : 0.0;
+		double py = do_y ? normal_dist(generator) : 0.0;
+		double pz = do_z ? normal_dist(generator) : 0.0;
+		return std::make_tuple(px,py,pz);
+	}
+
+	std::tuple<double,double,double> generate_sample_Shell()
+	{
+		double pMag   = pMag_dist(generator);
+		double pTheta = pTheta_dist(generator);
+		double pPhi   = pPhi_dist(generator);
+		double sin_pTheta = sin(pTheta);
+		double px = pMag*sin_pTheta*cos(pPhi);
+		double py = pMag*sin_pTheta*sin(pPhi);
+		double pz = pMag*cos(pTheta);
+		return std::make_tuple(px,py,pz);
+	}
+
+public:
+
+	distribution_3D( const string & mode, int seed,
+				param_list & parameters,
+				ostream & out_stream = std::cout,
+				ostream & err_stream = std::cerr )
+				:
+				out(out_stream),
+				err(err_stream),
+				generator( default_random_engine(seed) )
+				{
+					if (mode == "Gaussian")
+					{
+						scale = std::get<double>(parameters.at("scale"));
+						do_x  = std::get<bool>(parameters.at("do_x"));
+						do_y  = std::get<bool>(parameters.at("do_y"));
+						do_z  = std::get<bool>(parameters.at("do_z"));
+						normal_dist = normal_distribution<double>(0.0, scale);
+						generate_sample = [this]{ return generate_sample_Gaussian(); };
+					}
+					else if (mode == "Shell")
+					{
+						scale = std::get<double>(parameters.at("scale"));
+						width = std::get<double>(parameters.at("width"));
+						pMag_dist = uniform_real_distribution<double>(scale-width, scale+width);
+						pTheta_dist = uniform_real_distribution<double>(0.0, M_PI);
+						pPhi_dist = uniform_real_distribution<double>(-M_PI, M_PI);
+						generate_sample = [this]{ return generate_sample_Shell(); };
+					}
+					else
+					{
+						err << "Invalid mode = " << mode << endl;
+						std::terminate();
+					}
+				}
+
+	~distribution_3D(){}
+
+  std::function<std::tuple<double,double,double>()> generate_sample;
+
+};
+
+#endif
--- include/random_events.h
+++ include/random_events.h
@@ -8,6 +8,7 @@
 #include <chrono>
 #include <random>
 
+#include "./distribution_3D.h"
 #include "./ParticleRecord.h"
 #include "./ParameterReader.h"
 
@@ -24,8 +25,7 @@ namespace shift_lib
 
 		double mass 	= paraRdr->getVal("mass");
 		double RNG_R 	= paraRdr->getVal("RNG_R");
-		// double RNG_a 	= paraRdr->getVal("RNG_a");
-		double RNG_p0 = paraRdr->getVal("RNG_p0");
+		double RNG_p0 	= paraRdr->getVal("RNG_p0");
 
 		int RNG_Nev 	= paraRdr->getVal("RNG_Nev");
 		int RNG_mult 	= paraRdr->getVal("RNG_mult");
@@ -35,17 +35,18 @@ namespace shift_lib
 
 		int RNG_seed	= (int)paraRdr->getVal("RNG_seed");
 
-		unsigned seed = chrono::system_clock::now().time_since_epoch().count();
-		default_random_engine generator;
-		if ( RNG_seed < 0 )
-			generator = default_random_engine (seed);
-		else
-			generator = default_random_engine (RNG_seed);
+		unsigned time_seed = chrono::system_clock::now().time_since_epoch().count();
+		int seed_to_use = ( RNG_seed < 0 ) ? time_seed : RNG_seed;
+		default_random_engine generator = default_random_engine (seed_to_use);
 
-// cout << "RNG_seed = " << RNG_seed << endl;
-
-		// normal_distribution<double> distribution(0.0, RNG_R);
 		normal_distribution<double> distribution(0.0, 1.0);
+
+		// use the 3D distribution here
+		param_list parameters ( { {"scale", RNG_p0},
+															{"do_x", (bool)RNG_xDir},
+															{"do_y", (bool)RNG_yDir},
+															{"do_z", (bool)RNG_zDir} } );
+		distribution_3D dist3D( "Gaussian", seed_to_use, parameters );
 
 		//for (int iEvent = 0; iEvent < RNG_Nev; ++iEvent)
 		//{
@@ -59,12 +60,13 @@ namespace shift_lib
 				double yP = RNG_yDir ? RNG_R * distribution(generator) : 0.0;
 				double zP = RNG_zDir ? RNG_R * distribution(generator) : 0.0;
 
-				// double px = RNG_xDir ? RNG_a * distribution(generator) : 0.0;
-				// double py = RNG_yDir ? RNG_a * distribution(generator) : 0.0;
-				// double pz = RNG_zDir ? RNG_a * distribution(generator) : 0.0;
-				double px = RNG_xDir ? RNG_p0 * distribution(generator) : 0.0;
-				double py = RNG_yDir ? RNG_p0 * distribution(generator) : 0.0;
-				double pz = RNG_zDir ? RNG_p0 * distribution(generator) : 0.0;
+				// double px = RNG_xDir ? RNG_p0 * distribution(generator) : 0.0;
+				// double py = RNG_yDir ? RNG_p0 * distribution(generator) : 0.0;
+				// double pz = RNG_zDir ? RNG_p0 * distribution(generator) : 0.0;
+				// std::tie(px,py,pz)
+				auto [px,py,pz] = dist3D.generate_sample();
+cout << "px,py,pz = " << px << "   " << py << "   " << pz << endl;
+if (1) std::terminate();
 				double Ep = sqrt( mass*mass + px*px + py*py + pz*pz );
 
 				ParticleRecord particle;
