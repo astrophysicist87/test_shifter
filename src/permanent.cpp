c8c48c506696370413b82ea52def5570fd8ffd0f
Christopher Plumberg <astrophysicist87@gmail.com>
2023-06-14 18:20:48 -0700

    Update permanent.cpp

--- src/permanent.cpp
+++ src/permanent.cpp
@@ -301,6 +301,7 @@ double MatrixPermanent::compute_permanent_from_cluster(
 		A_sorted[i*n+j] = A[indices[i]*n+indices[j]];
 
 	A = A_sorted;
+	std::reverse(A.begin(), A.end());
 
 	vector<long> sorted_rowsums = get_rowsums( A, n );
 	if (n>=CUTOFF)
