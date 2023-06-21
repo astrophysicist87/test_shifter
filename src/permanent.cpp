ffdc5dbddc9f1f21977742e01cc72116af63b9fa
Christopher Plumberg <astrophysicist87@gmail.com>
2023-06-21 09:49:32 -0700

    Update permanent.cpp

--- src/permanent.cpp
+++ src/permanent.cpp
@@ -147,8 +147,8 @@ if (abs(BE_distance[UTindexer(pair.ID1, pair.ID2, total_n)]-tmp) > 1e-10)
   cout << "Particle 1: " << pair.p1 << "\n";
   cout << "Particle 2: " << pair.p2 << "\n";
   cout << tmp << "  " << UTindexer(pair.ID1, pair.ID2, total_n);
-  cout << "  " << BE_distance[UTindexer(pair.ID1, pair.ID2, total_n)] << "  ";
-  cout << abs(tmp-BE_distance[UTindexer(pair.ID1, pair.ID2, total_n)]) << "\n\n";
+  cout << "  " << BE_distance.at(UTindexer(pair.ID1, pair.ID2, total_n)) << "  ";
+  cout << abs(tmp-BE_distance.at(UTindexer(pair.ID1, pair.ID2, total_n))) << "\n\n";
   cout << "CHECK: " << pair.ID1 << "  " << pair.ID2 << "  " << BE_distance[UTindexer(pair.ID1, pair.ID2, total_n)] << endl;
 
 	// cout << "======================================================================================\n";
